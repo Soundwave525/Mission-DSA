76eb37cd225d323901af8780ce0ef94b9a4fe65b
Waleed Iqbal <2k23.aids2310712@gmail.com>
2025-08-25 10:15:33 +0530

    Create 1700-Number of Students Unable to Eat Lunch.cpp

---
+++ Leetcode/1700-Number of Students Unable to Eat Lunch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int count0 = 0;
+        for (int s : students) {
+            if (s == 0) count0++;
+        }
+        int count1 = students.size() - count0;
+
+        for (int s : sandwiches) {
+            if (s == 0) {
+                if (count0 == 0) {
+                    return count1;
+                }
+                count0--;
+            } else { 
+                if (count1 == 0) {
+                
+                    return count0;
+                }
+                count1--;
+            }
+        }
+      
+        return 0;
+    }
+};
