7ae0462eab5a6382c307422f4dfa274fbd43e3c5
Waleed Iqbal <2k23.aids2310712@gmail.com>
2025-09-04 10:10:53 +0530

    Update Queen's Attack II.py

--- hackerrank/Queen's Attack II.py
+++ hackerrank/Queen's Attack II.py
@@ -1,1 +1,18 @@
+def queensAttack(n, k, r_q, c_q, obstacles):
+  o_set = set((r, c) for r, c in obstacles)
 
+  directions = [ (0, 1), (0, -1), (1, 0), (1, -1), (1, 1), (-1, 0), (-1, 1), (-1, -1) ]
+
+  count  = 0
+
+  for dr, dc in directions:
+    row, col = r_q + dr, r_c + dc
+
+    while 1 <= row <= n and 1 <= col <= n:
+      if (row, col) in o_set:
+        break
+      count += 1
+      row += dr
+      col += dc
+
+  return count
